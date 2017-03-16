2f3a037cfe49757b657c29a5b6eb2c5b5cc6dbb1
NerdSier <493012328@qq.com>
2017-03-16 16:34:13 +0800

    s
    ss

---
+++ Models/PFPerson.swift
@@ -0,0 +1,13 @@
+//
+//  PFPerson.swift
+//  PFGame
+//
+//  Created by dev on 17/3/16.
+//  Copyright © 2017年 WPF. All rights reserved.
+//
+
+import UIKit
+
+class PFPerson: NSObject {
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PFGame
+//
+//  Created by dev on 17/3/16.
+//  Copyright © 2017年 WPF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
